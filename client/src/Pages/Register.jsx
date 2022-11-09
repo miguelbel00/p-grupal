8c28de8b722129316b3015f0f0ff4b48bed8bd1e
Emi <emilianogarciaokk@gmail.com>
2022-11-09 17:05:54 -0300

    Register

--- client/src/Pages/Register.jsx
+++ client/src/Pages/Register.jsx
@@ -2,12 +2,25 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
 import { useState } from "react";
+import postUser from "../redux/actions";
+import logo from '.././assets/logo.png'
+import '../styles/register.css'
 
 export default function Register() {
+
+const dispatch=useDispatch()
 const[input,setInput]=useState({
+fullname:"",
 email:"",
-password:""
+password:"",
+address:"",
+phone:""
 })
+
+function validateInput(input){
+    if(!input.email)return "Por Favor completa los campos requeridos"
+    }
+    const errormessage=validateInput(input)
 
 function handleChangue(e){
     e.preventDefault()
@@ -17,17 +30,22 @@ function handleChangue(e){
     })
 }
 
-
 function handleSubmit(e){
-e.preventDefault()
+// dispatch(postUser(e))
+if(errormessage){
+    return alert('Por favor,ingrese un correo y contraseña validos para registrarse!')
+}else{
+    return alert('Tu cuenta ha sido creada con exito')
+}
 }
 
-function validateInput(input){
-
+function buttonError(errormessage){
+if(errormessage){
+return <button  type="disabled" className="errorButton">Register!</button>
+}else{
+return <button className="godButton">Register!</button>  
 }
-const errormessage=validateInput(input)
-
-
+}
 
 
 
@@ -39,21 +57,31 @@ return(
 <div className="divMayorRegister">
     <div className="divHijoRegister">
         <div className="nameRegister">
-            <h3>Name of Page</h3>
+            <img src={logo} alt="Imagen not found"width='200px'height='200px' />
         </div>
-        <br />
         <div className="form">
-            <form>
+            <form onSubmit={e=>handleSubmit(e)}>
                 <div>
-                    <input type="text" name="email" value={input.email}  className="registerEmail" placeholder="Email"onChange={e=>handleChangue(e)} />
+                    <input className="inputStyle" type="text" name="fullname"value={input.fullName} placeholder='FullName' onChange={e=>handleChangue(e)}/>
                 </div>
                 <div>
-                    <input  className="registerPassword" type="text" name="password"value={input.password} placeholder="Password"onChange={e=>handleChangue(e)}/>
+                    <input type="text" name="email" value={input.email}  className="inputStyle" placeholder="Email"onChange={e=>handleChangue(e)} />
                 </div>
-                {errormessage&&(
-                <button className="botonRegister">Registrar</button>
-                )
+                <div>
+                    <input  className="inputStyle" type="text" name="password"value={input.password} placeholder="Password"onChange={e=>handleChangue(e)}/>
+                </div>
+                <div>
+                    <input type="text" className="inputStyle" name="address"value={input.address}placeholder='Address'onChange={e=>handleChangue(e)} />
+                </div>
+                <div>
+                    <input type="text" className="inputStyle" name="phone" value={input.phone}placeholder="Phone"onChange={e=>handleChangue(e)} />
+                </div>
+                <br />
+                {
+                buttonError(errormessage)
                 }
+                <p>Ya tienes cuenta?<Link to='/login'>Inicia Sesion</Link></p>
+                {/* <p>Olvidaste tu clave?<Link to='/recuperationPassword'>Recuperala Aqui!</Link></p> */}
             </form>
         </div>
     </div>
