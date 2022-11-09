7486f11a9647c098487c69ce2d988ae4a6b698c6
Ariel-Alegre <arielalegre98@gmail.com>
2022-11-09 15:25:24 -0300

    route product GET

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,9 +1,10 @@
 const express = require('express')
-const { get } = require('../controllers/index')
+const { get } = require('../controllers/products/products')
 
 const router = express.Router()
 
 // example of a route with index controller get function
-router.get('/', get)
+//router.get('/', get)
+router.get('/products', get)
 
 module.exports = router
