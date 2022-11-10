99915f043ea34ddc2cbbba60406bc09ff9c11cdb
gabicasas01 <casas.gf7@gmail.com>
2022-11-10 10:18:56 -0300

    feat(navbar): chenges

--- client/src/componets/NavBar.jsx
+++ client/src/componets/NavBar.jsx
@@ -1,39 +1,18 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import Styles from '../styles/navbar.module.css'
 import astroLogo from '../assets/astrologo2.0(sin fondo).png'
+import Styles from '../styles/navbar.module.css'
 
 
 const Navbar = () => {
 
     return (
-        // <div className={Styles.navbar_container}>
-            // <Link to='/' >
-            //     <div className='navbar_logo_container'>
-            //         <img src={astroLogo} className={Styles.logo} alt='astro_logo' />
-            //     </div>
-            // </Link>
-            // <div>
-            //     <input 
-            //         className='input'
-            //         type='text'
-            //         placeholder='Buscar...'
-            //     />
-            //     <button>Buscar</button>
-            // </div>
-            // <div className='navbar_buttons'>
-            //     <button>Mi cuenta</button>
-            //     <button>Mi carrito</button>
-            // </div>
-        // </div>
-
         <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
             <div className="container-fluid">
                 <div>
                     <Link to='/' >
                         <img src={astroLogo} className={Styles.logo} alt='astro_logo'/>
-                        {/* <a className="navbar-brand" href="#">Astro Games</a> */}
-                        <a className={Styles.navbar_brand} >Astro Games</a>
+                        <a className={Styles.navbar_brand}>Astro Games</a>
                     </Link>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
@@ -43,14 +22,18 @@ const Navbar = () => {
                     <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                         <li className="nav-item">
                             <Link to='/' >
-                                <a className="nav-link" aria-current="page" href="#">Inicio</a>
+                                <p className="nav-link">Inicio</p>
                             </Link>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link" href="#">Mi cuenta</a>
+                            <Link to='/' >
+                                <p className="nav-link">Mi cuenta</p>
+                            </Link>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link" href="#">Mi carrito</a>
+                            <Link to='/' >
+                                <p className="nav-link">Mi carrito</p>
+                            </Link>
                         </li>
                     </ul>
                     <form className="d-flex" role="search">
