064e026ee5833ecc008a53ff4ed1417887f02859
FranzDuran <franzduran.fd@gmail.com>
2022-11-10 10:49:29 -0300

    create user controllers and fix model user

--- api/controllers/UserControllers/UserCreate.js
+++ api/controllers/UserControllers/UserCreate.js
@@ -1,36 +1,38 @@
 const {ErrorObject} = require("../../helpers/error")
 const createHttpError = require('http-errors')
-const { User } = require('../database/models')
-const { endpointResponse } = require('../helpers/success')
+const { User } = require('../../database/models')
+const { endpointResponse } = require('../../helpers/success')
 
 // example of a controller. First call the service, then build the controller method
 module.exports = {
   userCreate: async (req, res, next) => {
-    const { fullname, email, password, phone } = req.body;
+    const { fullName, email, password, phone } = req.body;
     try{
       const userEmail = await User.findAll({
         where: {
           email: email
             }
       })
-      if(userEmail){
+    
+      if(userEmail.length){
           throw new ErrorObject("That email is already in use", 400)
       }
-      
+    
       const response = await User.create({
-        fullname, email, password, phone
+        fullName, email, password, phone
       })
+      
       endpointResponse({
         res,
         code:201,
-        message: 'Test retrieved successfully',
+        message: 'User retrieved successfully',
         body: response,
       })
 
     }catch(error){
         const httpError = createHttpError(
             error.statusCode,
-            `[Error get all users] - [UserCreateControllers - POST]: ${error.message}`,
+            `[Error create users] - [UserCreateControllers - POST]: ${error.message}`,
           )
           next(httpError)
     }
