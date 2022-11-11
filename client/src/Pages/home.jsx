8fa493f0ae254199d5581c2c9ca084cf7641fc1f
lucaaramos <lucaramos99@gmail.com>
2022-11-11 16:11:04 -0300

    feat(Home):created Home

--- client/src/Pages/home.jsx
+++ client/src/Pages/home.jsx
@@ -10,16 +10,16 @@ export default function Home(){
         <Carrusel/>
             <div className="conteiner">
                     <div className="div1">
-                        <h2 className="h2">Pc</h2>
+                        <h3>Computadoras</h3>
                         <img className="img" src="https://app.contabilium.com/files/explorer/16277/Productos-Servicios/concepto-7930450.jpg" alt="blank"/>        
                     </div>
                     <div className="div1">
-                        <h2 className="h2">Components</h2>
+                        <h3>Componentes</h3>
                         <img className="img" src="https://www.fullh4rd.com.ar/img/productos/Pics_Prod/video-geforce-gtx-1660-6gb-gigabyte-oc-nv-0.jpg" alt="blank" />
                     </div>
                     <div className="div1">
-                        <h2 className="h2">Peripheral</h2>
-                        <img className="img"  src="https://compucordoba.com.ar/img/Public/1078-producto-auriculares-msi-immerse-gh50-pc-consolas-11-9692.jpg" alt="blank"/>
+                        <h3>Perifericos</h3>
+                        <img className="img" src="https://compucordoba.com.ar/img/Public/1078-producto-auriculares-msi-immerse-gh50-pc-consolas-11-9692.jpg" alt="blank"/>
                     </div> 
             </div>
     </div>
