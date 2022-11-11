6141b24a122a21890ecf5b98080096af5ef06e24
Ariel-Alegre <arielalegre98@gmail.com>
2022-11-11 09:01:48 -0300

    routes products POST

--- api/routes/CreateProductRoutes.js
+++ api/routes/CreateProductRoutes.js
@@ -1,7 +1,7 @@
 
 const express = require('express');
 const router = express.Router();
-const { createProducts } = require('../controllers/ProductCreateControllers/productCreate')
+const { createProducts } = require('../controllers/Product/productCreate')
 
 router.post('/', createProducts)
 
--- api/routes/UserRoutes.js
+++ api/routes/UserRoutes.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const { userCreate } = require('../controllers/UserControllers/UserCreate')
+const { userCreate } = require('../controllers/User/UserCreate')
 
 const router = express.Router()
 
