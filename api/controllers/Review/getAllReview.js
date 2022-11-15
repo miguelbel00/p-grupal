ab291ca0d9f78689a584da0f17c21946fc85c007
unknown <miguel00@hotmail.com>
2022-11-14 23:31:13 -0500

    feat(Review): add get All reviews Route

---
+++ api/controllers/Review/getAllReview.js
@@ -0,0 +1,28 @@
+const {ErrorObject} = require("../../helpers/error")
+const createHttpError = require('http-errors')
+const { Review } = require('../../database/models')
+const { endpointResponse } = require('../../helpers/success')
+
+module.exports = {
+  getAllReview: async (req, res, next) => {
+    
+    try{
+        
+      const response = await Review.findAll()
+      
+      endpointResponse({
+        res,
+        code:201,
+        message: 'All Reviews',
+        body: response,
+      })
+
+    }catch(error){
+        const httpError = createHttpError(
+            error.statusCode,
+            `[Error Get All Reviews] - [GetAllReviewControllers - GET]: ${error.message}`,
+          )
+          next(httpError)
+    }
+  }
+}
