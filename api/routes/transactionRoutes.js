d9953f2d5461d8d1f3964e0e5e9a528c7a9b260e
unknown <miguel00@hotmail.com>
2022-11-17 11:45:37 -0500

    intent create files with conflicts

---
+++ api/routes/transactionRoutes.js
@@ -0,0 +1,8 @@
+const express = require('express')
+const {createTransaction} = require('../controllers/Transaction/transactionCreate')
+
+const router = express.Router()
+
+router.post('/',createTransaction);
+
+module.exports = router;
---
+++ api/routes/userRoutes.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const { editUser } = require('../controllers/User/userEdit')
+const {getAllUser} = require('../controllers/User/getAllUsers')
+
+
+
+const router = express.Router()
+
+router.put('/',editUser)
+router.get('/',getAllUser)
+
+
+module.exports = router
