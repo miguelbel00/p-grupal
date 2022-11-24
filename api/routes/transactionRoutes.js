15f97f392362cf8e7eae7f5e108898547b081f24
Ariel-Alegre <arielalegre98@gmail.com>
2022-11-24 11:45:55 -0300

    all transaction GET

--- api/routes/transactionRoutes.js
+++ api/routes/transactionRoutes.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const { createTransaction } = require('../controllers/Transaction/transactionCreate')
 const { transactionUpdate } = require('../controllers/Transaction/transactionUpdate.js')
-
+const { getAllTransaction } = require('../controllers/Transaction/getAlltransaction')
 const router = express.Router()
 
 router.post('/', createTransaction)
 router.put('/', transactionUpdate)
+router.get('/', getAllTransaction)
+
 
 module.exports = router
