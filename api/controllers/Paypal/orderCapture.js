890bc8ab17ef177ddf86043442c920a7564e1fc2
agustinhummel <agustinhummel7@gmail.com>
2022-11-25 18:07:36 -0300

    agregado: restar el stock y aumentar los vendidos

--- api/controllers/Paypal/orderCapture.js
+++ api/controllers/Paypal/orderCapture.js
@@ -1,13 +1,15 @@
 // CAMPTURADOR DE ORDER
-const axios = require('axios')
+const axios = require('axios');
+const e = require('express');
 const createHttpError = require('http-errors');
-const { Transaction, User } = require('../../database/models');
+const { Transaction, User, Product } = require('../../database/models');
 module.exports = {
     orderCapture: async (req, res, next) => {
 
         try {
-            const { token, description, userId, productsId } = req.query
+            let { token, description, userId, productsId, stock, sold,totalCart } = req.query
             
+
             const response = await axios.post(`${process.env.PAYPAL_API}/v2/checkout/orders/${token}/capture`, {}, {
                 auth: {
                     username: process.env.PAYPAL_API_CLIENT,
@@ -21,22 +23,31 @@ module.exports = {
                 value: parseFloat(response.data.purchase_units[0].payments.captures[0].amount.value),
                 status: 'Completed'
             }
+
             const value = detailTrans.value
             const {email, fullName} = await User.findByPk(userId)
 
             const products_ID = productsId.split(',')
 
             const createTransaction = await Transaction.create(detailTrans)
+            if (totalCart != 'undefined') {
+                
+                totalCart = JSON.parse(totalCart)
+                for (const key in totalCart) {
+                    let temp = await Product.findByPk(key)
+                    temp.update({stock:(temp.dataValues.stock-totalCart[key][1]),sold:(temp.dataValues.sold+totalCart[key][1])})
+                }
+            }else{
+                let temp = await Product.findByPk(productsId)
+                temp.update({stock:(temp.dataValues.stock-1),sold:(temp.dataValues.sold+1)})
+            }
 
             products_ID.map( (e)=>  createTransaction.addProducts(parseInt(e)))
             
-            axios.post(`${process.env.BACK_URL}/email`,{email, fullName, description, value})
-
+            axios.post(`${process.env.BACK_URL}/email`,{email, fullName, description, value,createTransaction})
 
             res.redirect(process.env.FRONT_URL)
 
-
-            
 
         } catch (error) {
             const httpError = createHttpError(
@@ -45,7 +56,6 @@ module.exports = {
             )
             next(httpError)
         }
-
 
     }
 }
