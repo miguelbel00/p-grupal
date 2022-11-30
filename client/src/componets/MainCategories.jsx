0aac5812f651f51635626c73da8ae66bf5a351c8
gabicasas01 <casas.gf7@gmail.com>
2022-11-30 11:54:20 -0300

    main categories logica

--- client/src/componets/MainCategories.jsx
+++ client/src/componets/MainCategories.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
+import { useDispatch, useSelector } from 'react-redux'
 import Styles from "../styles/home.module.css"
+import {  filterCategory, addCategorieFilter} from "../redux/actions/actionsFilter";
 import mainCategories_computers from '../assets/main_cateogires_computers.jpeg'
 import mainCategories_components from '../assets/main_cateogires_componentes.jpeg'
 import mainCategories_perifericos from '../assets/main_cateogires_perifericos.jpeg'
@@ -8,22 +10,27 @@ import mainCategories_perifericos from '../assets/main_cateogires_perifericos.jp
 
 
 function MainCategories() {
+    const dispatch = useDispatch()
+    const history = useHistory()
+
+    function handleFilterCategory(e) {
+        e.preventDefault();
+        dispatch(addCategorieFilter([e.target.name]))
+        dispatch(filterCategory())
+        history.push('/products')
+    }
+    
+
   return (
     <div className={Styles.category}>
         <div>
-            <Link to={'/products'} className="text-reset text-decoration-none" >
-                <img src={mainCategories_computers} alt="blank" />
-            </Link>
+            <img src={mainCategories_computers} alt="blank" onClick={handleFilterCategory} name='Computadores'/>
         </div>
         <div>
-            <Link to={'/products'} className="text-reset text-decoration-none" >
-                <img src={mainCategories_components} alt="blank" />
-            </Link>
+            <img src={mainCategories_components} alt="blank" onClick={handleFilterCategory} name='Componentes'/>
         </div>
         <div>
-            <Link to={'/products'} className="text-reset text-decoration-none" >
-                <img src={mainCategories_perifericos} alt="blank" />
-            </Link>
+            <img src={mainCategories_perifericos} alt="blank" onClick={handleFilterCategory} name='Perifericos'/>
         </div>
     </div>
   )
